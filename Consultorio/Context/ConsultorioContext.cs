ce8b2a5e2ddfe84d37a10d3790ab0e80ccbb09d2
Moisés Sansigolo <moises.sansigolo@gmail.com>
2022-06-28 11:59:07 -0300

    Adicionando banco de dados

---
+++ Consultorio/Context/ConsultorioContext.cs
@@ -0,0 +1,15 @@
+﻿using Consultorio.Models.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Consultorio.Context
+{
+    public class ConsultorioContext : DbContext
+    {
+        public ConsultorioContext(DbContextOptions<ConsultorioContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Agendamento> Agendamentos { get; set; }
+    }
+}
--- Consultorio/Startup.cs
+++ Consultorio/Startup.cs
@@ -1,16 +1,12 @@
+using Consultorio.Context;
 using Consultorio.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Consultorio
 {
@@ -28,6 +24,12 @@ namespace Consultorio
         {
 
             services.AddControllers();
+            services.AddDbContext<ConsultorioContext>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("Default"));
+             });
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultorio", Version = "v1" });
