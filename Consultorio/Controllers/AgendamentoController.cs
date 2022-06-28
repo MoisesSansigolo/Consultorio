d14c42a329f8500d9041084e77a4780eac220cb6
Moisés Sansigolo <moises.sansigolo@gmail.com>
2022-06-28 11:14:06 -0300

    Adicionar arquivos de projeto.

---
+++ Consultorio/Controllers/AgendamentoController.cs
@@ -0,0 +1,52 @@
+﻿using Consultorio.Models.Entites;
+using Consultorio.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        List<Agendamento> agendamentos = new List<Agendamento>();
+
+        public AgendamentoController(IEmailService emailService)
+        {
+            agendamentos.Add(new Agendamento { Id = 1, NomePaciente = "Jonathan", Horario = new DateTime(2022, 07, 18)});
+
+            _emailService = emailService;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(agendamentos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+           var agendamentoSelecionado = agendamentos.FirstOrDefault(x => x.Id == id);
+            return agendamentoSelecionado != null ? Ok(agendamentoSelecionado) : BadRequest("Erro ao buscar o agendamento");
+
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var pacienteAgendado = true;
+
+            if (pacienteAgendado)
+            {
+                _emailService.EnviarEmail("moises.sansigolo@gmail.com");
+            }
+        return Ok();
+        }
+    }
+       
+}
---
+++ Consultorio/Models/Entites/Agendamentos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Models.Entites
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public string NomePaciente { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
